64c0e8bf528696baa2b96784beb198c5668d4520
Debra Peri <dperi@xion.org>
2020-01-01 13:06:21 -0800

    Update process_all_csv_files.bash

--- process_all_csv_files.bash
+++ process_all_csv_files.bash
@@ -13,7 +13,7 @@ do
  	echo $f
 
 
-# ./analyze_muse_data.py -db -f -ft 1  -hc 20 -lc 5 -o 8 -v 3 -c /Users/dperi/Documents/Muse_Dev/analyze_eeg/data-for-testing.csv
+# ./analyze_muse_data.py -hdf5 -db -f -ft 1  -hc 20 -lc 5 -o 8 -v 3 -c /Users/dperi/Documents/Muse_Dev/analyze_eeg/data-for-testing.csv
 
  	analyze_meditation_data.py -f -lc 1. -hc 70.0 -o $filter_order -p -e -l 2 -c $f
 # 	analyze_meditation_data.py -f -lc 1. -hc 70.0 -o $filter_order -p -e -l 2 -i -s 4 -c $f
