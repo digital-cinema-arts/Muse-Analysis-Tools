e836e6fc8c2c23778f27f2896d7876ba2210c140
Debra Peri <dperi@xion.org>
2020-01-02 11:43:32 -0800

    Create install_libraries.bash

---
+++ install_libraries.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+pip3 -v install time
+pip3 -v install datetime 
+
+pip3 -v install numpy 
+pip3 -v install scipy
+pip3 -v install math
+pip3 -v install bitstring
+pip3 -v install pandas 
+pip3 -v install os
+pip3 -v install sleep
+pip3 -v install strftime
+pip3 -v install gmtime
+pip3 -v install sys
+pip3 -v install csv
+pip3 -v install argparse
+pip3 -v install math
+pip3 -v install tqdm
+pip3 -v install progress.bar
+pip3 -v install json
+pip3 -v install h5py, tables
+pip3 -v install pathlib
+pip3 -v install matplotlib
+
+pip3 -v install matplotlib.pyplot 
+pip3 -v install matplotlib.ticker 
+
+pip3 -v install PyQt5.QtWidgets
+pip3 -v install PyQt5.QtGui
+
+pip3 -v install PyQt5.QtCore
+pip3 -v install PyQt5.QtWidgets
